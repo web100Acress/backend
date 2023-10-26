538175cdefb138da2a81d806379bc1bc83bb4129
amitchaud10@gmail.com <amitchaud10@gmail.com>
2023-10-26 15:42:22 +0530

    db

--- Controller/AdminController/FrontController/NewlauchController.js
+++ Controller/AdminController/FrontController/NewlauchController.js
@@ -956,7 +956,9 @@ class newlaunchController {
             })
         } catch (error) {
         console.log(error)
-        res.status(500)
+        res.status(500).json({
+            message:"Internal server error !"
+        })
         }
     }
 
