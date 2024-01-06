9cf42d821a8a686c43d947d84c13c018bab961f6
amitchaud10@gmail.com <amitchaud10@gmail.com>
2024-01-06 15:19:28 +0530

    hello

--- middleware/registerAuth.js
+++ middleware/registerAuth.js
@@ -1,16 +1,17 @@
 const jwt = require('jsonwebtoken')
+const postPropertyModel = require('../models/postProperty/post')
 
-const postPropertyModel=require('../models/postProperty/post')
+// const postPropertyModel=require('../models/postProperty/post')
 
 const authAdmin = async (req, res, next) => {
     try {
         // res.send("hello auth")
         const { token } = req.cookies
-        //    res.send(token)
+           res.send(token)
 
         const verify_token = jwt.verify(token, 'amitchaudhary100')
-        // console.log(verify_token)
-        const admin_data = await registerModel.findOne({ _id: verify_token.user_id })
+        console.log(verify_token)
+        const admin_data = await postPropertyModel.findOne({ _id: verify_token.user_id })
         // console.log(admin_data)
         req.admin = admin_data
         next()
