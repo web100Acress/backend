9b4add1fbed596498fe1c37fa30c7dd8d9880ace
amitchaud10@gmail.com <amitchaud10@gmail.com>
2024-01-15 11:52:04 +0530

    helo

--- db/connect_db.js
+++ db/connect_db.js
@@ -5,8 +5,8 @@ const dotenv = require('dotenv').config()
 const connectDb=()=>{
 
   //  return mongoose.connect("mongodb://localhost:27017/100_Acr~e")
-  return mongoose.connect("mongodb://127.0.0.1:27017/100_Acre")
-  // return mongoose.connect("mongodb+srv://amit100acre:Amit123@cluster0.ffg8qyf.mongodb.net/?retryWrites=true&w=majority")
+  // return mongoose.connect("mongodb://127.0.0.1:27017/100_Acre")
+  return mongoose.connect("mongodb+srv://amit100acre:Amit123@cluster0.ffg8qyf.mongodb.net/?retryWrites=true&w=majority")
   .then(() =>{
     console.log('Connected!')
   })
