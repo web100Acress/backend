dc5e9acc9ed3d314952b786c623e480d891c53b0
100acress <seo.100acress@gmail.com>
2024-03-04 13:17:05 +0530

    todaymarch4

--- db/connect_db.js
+++ db/connect_db.js
@@ -9,7 +9,7 @@ const connectDb=()=>{
   // return mongoose.connect("mongodb+srv://web100acress:Amit100@cluster0.lcaufzm.mongodb.net/")
 
 
-    // return mongoose.connect("mongodb+srv://Amit:Amit123@cluster0.7jljtxl.mongodb.net/")
+    // return mongoose.connect("mongodb+srv://Amit:Amit123@cluster0.7jljtxl.mongodb.net/")li
   return mongoose.connect("mongodb+srv://amit100acre:Amit123@cluster0.ffg8qyf.mongodb.net/?retryWrites=true&w=majority")
   .then(() =>{
     console.log('Connected!')
