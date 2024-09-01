3bef4ac257f7cfbfe4f543780e7e9f21b7e9b26e
100acress <seo.100acress@gmail.com>
2024-09-01 11:46:55 +0530

    1 Sep 2024

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var cloudinary = require("cloudinary").v2;
 
 // Create a rate limit rule
 const limiter = rateLimit({
-  windowMs: 2 * 60 * 1000, // 15 minutes
-  max: 10, // Limit each IP to 100 requests per windowMs
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  max: 250, // Limit each IP to 100 requests per windowMs
   message: "Too many requests , please try again after sometime.",
 });
 // app.use(compression())
