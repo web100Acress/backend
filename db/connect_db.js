7b1390e137d7047d92035dbe6a1da2ff54a5f26a
shoppingGhar <shoppingcomplexdelhi@gmail.com>
2024-11-29 17:02:53 +0530

    hhfh

--- db/connect_db.js
+++ db/connect_db.js
@@ -9,10 +9,11 @@ const connectDb=()=>{
   // return mongoose.connect("mongodb+srv://web100acress:Amit100@cluster0.lcaufzm.mongodb.net/")
 
 
-    // return mongoose.connect("mongodb+srv://Amit:Amit123@cluster0.7jljtxl.mongodb.net/")li
-  return mongoose.connect(process.env.MONGO_URI||"mongodb+srv://amit100acre:Amit123@cluster0.ffg8qyf.mongodb.net/?retryWrites=true&w=majority")
+    // return mongoose.connect("mongodb+srv://Amit:Amit123@cluster0.7jljtxl.mongodb.net/")libj/lfb
+    // hkrehui
+  return  mongoose.connect(process.env.MONGO_URI||"mongodb+srv://amit100acre:Amit123@cluster0.ffg8qyf.mongodb.net/?retryWrites=true&w=majority")
   .then(() =>{
-    console.log('Connected!')
+    console.log('db Connected!')
   })
   .catch((error)=>{
     console.log(error)
