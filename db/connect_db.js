d722ae1736c377c2a332c9728ca94388604e2742
shoppingGhar <shoppingcomplexdelhi@gmail.com>
2024-12-09 17:28:32 +0530

    new update

--- db/connect_db.js
+++ db/connect_db.js
@@ -7,8 +7,6 @@ const connectDb=()=>{
   //  return mongoose.connect("mongodb://localhost:27017/100_Acre")
 
   // return mongoose.connect("mongodb+srv://web100acress:Amit100@cluster0.lcaufzm.mongodb.net/")
-
-
     // return mongoose.connect("mongodb+srv://Amit:Amit123@cluster0.7jljtxl.mongodb.net/")li
   return mongoose.connect(process.env.MONGO_URI||"mongodb+srv://amit100acre:Amit123@cluster0.ffg8qyf.mongodb.net/?retryWrites=true&w=majority")
   .then(() =>{
