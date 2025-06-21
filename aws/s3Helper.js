41886ad64ffdd0fd3c91479ad1574f51ea091975
web100Acress <158815978+web100Acress@users.noreply.github.com>
2025-06-21 18:18:17 +0530

    Update s3Helper.js

--- aws/s3Helper.js
+++ aws/s3Helper.js
@@ -3,7 +3,7 @@ const AWS = require("aws-sdk");
 require("dotenv").config();
 
 AWS.config.update({
-  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_S3_SECRET_ACESS_KEY,
   accessKeyId: process.env.AWS_S3_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
