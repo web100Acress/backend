51b1940686f3304bd02dce70ae286398adee0cc2
web100Acress <web.100acress@gmail.com>
2025-08-30 15:45:07 +0530

    sss

--- config/uploadLimits.js
+++ config/uploadLimits.js
@@ -5,7 +5,7 @@ const uploadLimits = {
   // File size limits based on environment
   fileSize: isDevelopment 
     ? 50 * 1024 * 1024  // 50MB in development
-    : 5 * 1024 * 1024,  // 5MB in production (safer for live servers)
+    : 10 * 1024 * 1024,  // 10MB in production (increased for project images)
     
   // Maximum files based on environment  
   maxFiles: isDevelopment ? 30 : 15,
