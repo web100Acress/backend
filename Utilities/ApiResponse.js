b519052c5b049f4a9af14b1a975e8287b1dece64
amantiwari2357 <amantiwariat2005@gmail.com>
2026-02-27 10:36:20 +0530

    backend

--- Utilities/ApiResponse.js
+++ Utilities/ApiResponse.js
@@ -7,4 +7,12 @@ class ApiResponse {
   }
 }
 
-module.exports = ApiResponse;
+const sendOk = (res, message, data = null) => {
+  return res.status(200).json({
+    success: true,
+    message,
+    data
+  });
+};
+
+module.exports = { ApiResponse, sendOk };
